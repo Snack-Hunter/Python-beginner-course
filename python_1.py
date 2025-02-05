66709f84bacc74a486d663b5c4cb45765eeb0718
Snack-Hunter <yonsnackchips@gmail.com>
2025-02-05 13:34:22 -0800

    git commit --allow-empty -m 'Initial empty commit'

---
+++ python_1.py
@@ -0,0 +1,12 @@
+def word_triangle(word):
+    length = len(word)
+    for ch in range(length):
+        print(word[:length - ch])
+        
+word_triangle("antidisestablishmentarianism")
+
+
+def startup_name(adverb):
+    print("the startup name is ",python3.6 --versionadverb + "ly")
+
+startup_name("compass")
